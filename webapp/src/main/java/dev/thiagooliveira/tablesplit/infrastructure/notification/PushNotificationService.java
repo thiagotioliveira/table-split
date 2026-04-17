a4c073edb8d6454139ed991c041822d4911f223f
Thiago Oliveira <thiago@thiagoti.com>
2026-04-17 11:46:44 +0100

    urgency high push notification (ttl 300s)

--- webapp/src/main/java/dev/thiagooliveira/tablesplit/infrastructure/notification/PushNotificationService.java
+++ webapp/src/main/java/dev/thiagooliveira/tablesplit/infrastructure/notification/PushNotificationService.java
@@ -72,7 +72,7 @@ public class PushNotificationService
               .userAuth(sub.getAuth())
               .payload(payload)
               .urgency(Urgency.HIGH)
-              .ttl(3600)
+              .ttl(300)
               .build();
       var response = pushService.send(notification, nl.martijndwars.webpush.Encoding.AES128GCM);
       int statusCode = response.getStatusLine().getStatusCode();
