e6849fc506ecf85212c6544475f8c52037459299
Thiago Oliveira <thiago@thiagoti.com>
2026-05-03 20:28:16 +0100

    telegram integration bug fix

--- webapp/src/main/java/dev/thiagooliveira/tablesplit/infrastructure/persistence/telegram/TelegramUserMappingJpaRepository.java
+++ webapp/src/main/java/dev/thiagooliveira/tablesplit/infrastructure/persistence/telegram/TelegramUserMappingJpaRepository.java
@@ -3,12 +3,16 @@ package dev.thiagooliveira.tablesplit.infrastructure.persistence.telegram;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface TelegramUserMappingJpaRepository
     extends JpaRepository<TelegramUserMappingEntity, Long> {
   List<TelegramUserMappingEntity> findByRestaurantId(UUID restaurantId);
 
+  @Modifying
+  @Transactional
   void deleteByChatId(Long chatId);
 }
